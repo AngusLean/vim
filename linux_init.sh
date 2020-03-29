aebb15ac1073ac6aa5b55dd2ad9b8d1d46b26d83
anguslean <cupofish@gmail.com>
2020-03-29 11:07:33 +0800

    update

---
+++ linux_init.sh
@@ -0,0 +1,6 @@
+sudo -i
+# install ctags
+apt-get install ctags
+# install rg
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
+sudo dpkg -i ripgrep_11.0.2_amd64.deb
